77cf83d91aa01d04b01d890d45a6be0fabf306fb
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-08 15:08:26 -0600

    feat: add Message Patterns

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsNumber, IsPositive } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+    // If it were apirest the following code would not be necessary
+    @IsNumber()
+    @IsPositive()
+    id: number;
+}
