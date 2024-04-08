cd4418f89f4a889f6e92901df6138edc57f4f4e5
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-08 14:13:55 -0600

    feat: add soft delete by id

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -23,14 +23,16 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     const { page, limit } = paginationDto;
     
-    const totalPages = await this.product.count()
+    const totalPages = await this.product.count({where: { available: true }})
 
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
         take: limit,
-        skip: (page - 1) * limit
+        skip: (page - 1) * limit,
+        where: { available: true }
+
       }),
       meta: {
         total: totalPages,
@@ -42,7 +44,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findOne(id: number) {
     const product = await this.product.findFirst({
-      where: {id}
+      where: { id, available: true },
     })
 
     if(!product) throw new NotFoundException(`Product with id #${id} not found`)
@@ -53,14 +55,27 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async update(id: number, updateProductDto: UpdateProductDto) {
     
     await this.findOne(id);
-    
+
     return this.product.update({
       where: {id},
       data: updateProductDto
     })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    // Hard Delete
+    // return  this.product.delete({
+    //   where: { id }
+    // })
+
+    const product = await this.product.update({
+      where: {id},
+      data: { 
+        available: false
+      }
+    })
+    return product; 
   }
 }
