95d354d9ba46d339de266f965e8a750a35954962
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-08 13:13:21 -0600

    feat: added pagination

---
+++ prisma/migrations/20240408160934_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "User";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -13,13 +14,30 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     this.logger.log('Database connected')
   }
   create(createProductDto: CreateProductDto) {
-
-    
-    return 'This action adds a new product';
+    return this.product.create({
+      data: createProductDto
+    })
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+
+    const { page, limit } = paginationDto;
+    
+    const totalPages = await this.product.count()
+
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        take: limit,
+        skip: (page - 1) * limit
+      }),
+      meta: {
+        total: totalPages,
+        page, 
+        lastPage
+      }
+    }
   }
 
   findOne(id: number) {
