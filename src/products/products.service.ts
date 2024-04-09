d91cbeed0209072a13079d9bbd13c6c2e2b613e0
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-09 17:01:29 -0600

    feat: CustomExceptionFilter

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -47,7 +48,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: { id, available: true },
     })
 
-    if(!product) throw new NotFoundException(`Product with id #${id} not found`)
+    if(!product) throw new RpcException({
+      message: `Product with id #${id} not found`,
+      statusCode: HttpStatus.NOT_FOUND
+    })
 
     return product;
   }
